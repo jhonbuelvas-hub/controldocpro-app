e27a4b30414d6c62acdaa661f014e2f615dd8bee
jhonbuelvas-hub <jhon.buelvas@gmail.com>
2026-03-23 10:10:54 -0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,30 @@
 from flask import Flask
+import os
+import psycopg2
 
 app = Flask(__name__)
 
+# Conexión a base de datos
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+def get_connection():
+    return psycopg2.connect(DATABASE_URL)
+
 @app.route("/")
 def home():
-    return "ControlDocPro funcionando 🚀"
+    return "ControlDocPro conectado a BD 🚀"
+
+@app.route("/test-db")
+def test_db():
+    try:
+        conn = get_connection()
+        cur = conn.cursor()
+        cur.execute("SELECT 1;")
+        result = cur.fetchone()
+        conn.close()
+        return f"Conexión exitosa: {result}"
+    except Exception as e:
+        return f"Error: {e}"
 
 if __name__ == "__main__":
     app.run(debug=True)
