c9b6c98f9de768a4ca016c6cbc288cd6e9fca09b
jhonbuelvas-hub <jhon.buelvas@gmail.com>
2026-04-07 12:26:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "✅ Server Flask funcionando desde Azure App Service"
+
+@app.route("/ping")
+def ping():
+    return jsonify({"status": "ok"})
